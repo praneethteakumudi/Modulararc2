8f0baa5400adb6f6c11fb4bcd90bb7689ac1f872
Rakesh V <rakesh@SUJA-NET.local>
2025-06-05 10:52:14 +0530

    Resolved merge conflict in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainmoduleModule } from './mainmodule/mainmodule.module';
-import { Navbar3Module } from './mainmodule/navbar3/navbar3.module';
+
 
 
 @NgModule({
@@ -13,11 +13,6 @@ import { Navbar3Module } from './mainmodule/navbar3/navbar3.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    MainmoduleModule,
-    Navbar3Module
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    AppRoutingModule]
 })
 export class AppModule { }
